b8e544089c4908ff583c075e815e160a50d3f403
Luke Evans <lukeevanstax@gmail.com>
2020-09-22 23:17:29 -0400

    make changes to index.js to pull in workout.js to render stats page

--- models/workout.js
+++ models/workout.js
@@ -3,50 +3,51 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutTracker = new Schema({
-  
+const schemaWorkout = new Schema({
+
     Days: {
         type: Date,
         default: Date.now
     },
-  
+
     exercises: [
-    {
-        type: {
-            type: String,
-            required: "Please enter a workout."
-        },
-        name: {
-            type: String,
-            required: "Please enter the name of the exercise."
-        },
-        duraction: {
-            type: Number,
-            required: "Please enter how long the workout lasted."
-        },
-        weight: {
-            type: Number
-        },
-        reps: {
-            type: Number
-        },
-        sets: {
-            type: Number
-        },
-        distance: {
-            type: Number
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Please enter a workout."
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Please enter the name of the exercise."
+            },
+            duraction: {
+                type: Number,
+                required: "Please enter how long the workout lasted."
+            },
+            weight: {
+                type: Number
+
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
         }
-    }
-        
     ]
 }, {
-   toJSON: {
-       virtuals: true
-   }
-
+    toJSON: {
+        virtuals: true
+    }
 });
-  
-const Workout = mongoose.model("Workout", WorkoutTracker);
+
+const workout = mongoose.model("Workout", schemaWorkout);
 
 // Export the User model
-module.exports = Workout;
+module.exports = workout;
--- routes/api-Routes.js
+++ routes/api-Routes.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const workout = require("../models/workout")
-const router = require ("express").Router();
+const router = require("express").Router();
 
 router.get("/api/workouts", function (req, res) {
     workout.find().then(function (dbWorkout) { res.json(dbWorkout) }).catch(err => res.json(err))
@@ -8,24 +8,28 @@ router.get("/api/workouts", function (req, res) {
 
 router.get("/api/workouts/range", function (req, res) {
     workout.find({}).limit(7).then(function (dbWorkout) { res.json(dbWorkout) }).catch(err => res.json(err))
+    // console.log(res)
 });
 
 router.post("/api/workouts", function (req, res) {
-    workout.create({}).then(function (dbWorkout) { res.json(dbWorkout) }).catch(err => res.json(err))
+    console.log(res);
+    workout.create({}).then(function (dbWorkout) {
+        res.json(dbWorkout)
+    }).catch(err => res.json(err))
+
 });
 
 router.put("/api/workouts/:id", function (req, res) {
+    // console.log(req.body)
     workout.findByIdAndUpdate(req.params.id, {
-        $push: { exercise: req.body }
-       
-    }
-        ,
+        $push: { exercises: req.body }  
+    },
         { new: true, runValidators: true }
     ).then(function (dbWorkout) { res.json(dbWorkout) }).catch(err => res.json(err))
 });
 
-router.delete("/api/workouts", function (req, res) {
-    workout.findByIdAndDelete(req.body.id).then(function () { res.json(true) }).catch(err => res.json(err))
-});
+// router.delete("/api/workouts", function (req, res) {
+//     workout.findByIdAndDelete(req.body.id).then(function () { res.json(true) }).catch(err => res.json(err))
+// });
 
 module.exports = router
